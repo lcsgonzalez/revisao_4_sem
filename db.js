90268ddac4366a282e6ee1c4485c1793f2a795b9
proflucasgonzalez <lucas.gonzalez@sp.senai.br>
2025-08-14 16:30:06 -0300

    Subindo o projeto completo

--- db.js
+++ db.js
@@ -6,7 +6,7 @@ const pool = mariadb.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'client_order_system',
-    port: process.env.DB_PORT || 3307,
+    port: process.env.DB_PORT || 3306,
     connectionLimit: 10
 });
 
